1244566549d5a6da411a972c55dda4d43b9e3137
ken <hitakatsukenichi@pco-MacBook-Air.local>
2020-07-31 19:36:59 +0900

    comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 class CommentsController < ApplicationController
 
   def new
+    @comment = Comment.new
   end
 
   def create
@@ -16,6 +17,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :post_id) #追加
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,14 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order(created_at: :desc)
-  
+
   end
 
   def show
     @post = Post.find_by(id: params[:id])
     @user = @post.user
+    @comments = @post.comments
+    @comment = Comment.new
   end
 
   def new
