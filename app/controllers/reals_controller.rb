27f87c0a7f20cefc4f724be390aa02e34faf19e0
Hida <pco@pco-MacBook-Air.local>
2021-12-21 18:55:40 +0900

    リアル作成

---
+++ app/controllers/reals_controller.rb
@@ -0,0 +1,4 @@
+class RealsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
 
 
+  
   get 'rooms/show'
   devise_for :users
 
   resources :users, only: [:show, :index]
   resources :posts, only: [:new, :create, :index, :show, :destroy]
   resources :comments, only: [:new, :create, :show]
+  resources :reals, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'posts#index'
   get  'users/:id/chat_index' => 'users#chat_index', as: 'chat_index'
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
   post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
   patch 'users/avater_update' => 'users#avater_update', as: 'avater_update'
-  patch 'users/bank_update' => 'users#bank_update', as: 'bank_update'  
+  patch 'users/bank_update' => 'users#bank_update', as: 'bank_update'
   post 'rooms/:to_user_id' => 'rooms#create'
 
   resources :rooms, only: [:show] # チャットルームの表示
