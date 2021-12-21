83780e680deb71e99a9f6a6a381dd66deb06c543
Hida <pco@pco-MacBook-Air.local>
2021-12-21 20:12:20 +0900

    リアル作成

--- app/controllers/reals_controller.rb
+++ app/controllers/reals_controller.rb
@@ -1,5 +1,18 @@
 class RealsController < ApplicationController
   def index
-    @reals = Real.all
+    @reals = Real.all.order(create_at: :desc)
+  end
+
+  def show
+    @real = Real.find_by(id: params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @real = Real.new(content: params[:content])
+    @real.save
+    redirect_to(reals_path)
   end
 end
