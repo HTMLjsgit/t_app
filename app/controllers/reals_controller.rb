fdb3a0fd9d2b05c9afe5299e9b459dcdeca04750
swappp0630 <swaappp0630@gmail.com>
2022-02-27 12:09:13 +0900

    change 3

--- app/controllers/reals_controller.rb
+++ app/controllers/reals_controller.rb
@@ -28,17 +28,24 @@ class RealsController < ApplicationController
   def create
     @real = Real.new(real_params)
     @real.save!
-
-    if params[:item][:images_attributes]
-      for i in 0..params[:item][:images_attributes].length - 1
-
-        @image_real = ImageReal.new(number: params[:item][:images_attributes].length,
-                          image_url: params[:item][:images_attributes][i][:image_url],
-                          picture: params[:item][:images_attributes][i][:image_url].read,
-                          real_id:  @real.id
-                          )
-        @image_real.save
+    if params[:item].present?
+      if params[:item][:images_attributes]
+        for i in 0..params[:item][:images_attributes].length - 1
+          @image_real = ImageReal.new(number: params[:item][:images_attributes].length,
+                            image_url: params[:item][:images_attributes][i][:image_url],
+                            picture: params[:item][:images_attributes][i][:image_url].read,
+                            real_id:  @real.id
+                            )
+          @image_real.save
+        end
       end
+    else
+      @image_real = ImageReal.new(number: 0,
+                            image_url: "",
+                            picture: "",
+                            real_id:  @real.id
+                            )
+      @image_real.save
     end
 
     redirect_to(reals_path)
