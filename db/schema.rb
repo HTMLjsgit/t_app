cd01b3496dbe8a9cfff1f552a92e52f8dbe383ad
Hida <pco@pco-MacBook-Air.local>
2022-05-13 17:58:42 +0900

    フッターアイコン

--- db/schema.rb
+++ db/schema.rb
@@ -33,19 +33,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_050317) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "username"
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "chat_posts", force: :cascade do |t|
     t.string "message"
     t.integer "user_id"
@@ -296,7 +283,6 @@ ActiveRecord::Schema.define(version: 2022_05_07_050317) do
     t.string "bank_account_type"
     t.string "bank_account_number"
     t.string "bank_account_horseman_name_kana"
-    t.boolean "isstopped", default: false, null: false
     t.string "background_image"
     t.boolean "admin", default: false, null: false
     t.string "avater"
